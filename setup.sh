5bce89582ce45f6fa8becdb307c0d00a743a8bb2
Josh Gitlin <jgitlin@pinnacle21.com>
2020-05-15 14:40:39 -0400

    Add some dependencies

--- setup.sh
+++ setup.sh
@@ -3,11 +3,16 @@
 set -e
 set -x
 
+# Install Development tools, needed for building some gems
+yum -y groupinstall "Development Tools"
+
 # Install Ruby and dependencies
 /bin/rpm -i /tmp/ruby-src.rpm
 /bin/rpm -i /tmp/ruby.rpm
 /bin/yum -y install curl make readline-devel ncurses-devel openssl-devel \
-      libyaml-devel libffi-devel zlib-devel
+      libyaml-devel libffi-devel zlib-devel libxml2-devel
+
+gem install bundler
 
 # Create a working directory
 mkdir -p /var/workdir
